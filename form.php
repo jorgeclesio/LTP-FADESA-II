c5bc885935d5cb33a3559b5efddb3e42b6284a6b
Jorge Clésio <jorgepebas@gmail.com>
2018-09-20 10:49:25 -0300

    Update form.php
    Atualizações de comentários de detalhes.

--- form.php
+++ form.php
@@ -6,21 +6,29 @@
 <input type="submit" name="submit">
 </form>
 <?php
-IF($_POST['submit']){ //
-$nome = $_FILES['arquivo']['name'];
-$tipo = $_FILES['arquivo']['type'];
-$size = $_FILES['arquivo']['size'];
-$tmp = $_FILES['arquivo']['tmp_name'];
-$dir='img/'.$nome;
-move_uploaded_file($tmp,$dir);
+IF($_POST['submit']){ //Verificação de ENvio
+$local=$_POST['local'];
+$nome = $_FILES['arquivo']['name']; //NOmedo Arquivo
+$tamanho = $_FILES['arquivo']['size']; //Tamanho em Bytes
+$tipo = $_FILES['arquivo']['type']; //Tipo do arquivos
+$tmp = $_FILES['arquivo']['tmp_name']; //Local temporário
 
+echo $local ."<br>";
 echo $nome."<br>";
-echo $tipo."<br>";
-echo $size."<br>";
+echo $tamanho."<br>";
 echo $tmp."<br>";
-}
+echo $tipo;
+
+$dir='imgg/'.$nome;
+if($tipo<>'image/gif'){ //Verifica o tipo de arquivo
+	if($tamanho<37000){
+	move_uploaded_file($tmp,$dir); //Mover Arquivo
+	} else {
+			echo "<br>Arquivo acima de 30 mil bytes..."; 
+			}
+} else {echo"Não é uma JPEG";}
+} // Final da verificação de envio.
 
 //echo time() .'<br>';
-
 //echo md5('lenicio.jpg'); //Cripitografia de dado
 ?>
